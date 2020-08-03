a85438c1a9a3cfb78028f39a54a8945d880d9cc2
Romano Lautaro <lautaro.romano@alu.frt.edu.ar>
2020-08-02 21:31:21 -0300

    Update WarCoin.cpp
    Ultimos cambios de diseño

--- WarCoin.cpp
+++ WarCoin.cpp
@@ -5,18 +5,22 @@ int Monedas();
 int Inicia();//para saber si inicia jugando la computadora o el jugador
 void Comienzo(int inicia,int cantmonedas);//Empieza el juego
 int CantMonedasQueRetiraLaPc();
+void Reglas();
 
 int min,max;
 main(){
+	Reglas();
 	printf("WARCOIN\n=========\n");
 	int CMonedas,PlayerOPc;
 	printf("Ingrese la cantidad minima de monedas que se pueden retirar: ");
 	scanf("%d",&min);
 	printf("\nIngrese la cantidad maxima de monedas que se pueden retirar: ");
 	scanf("%d",&max);
+	system("cls");
+	printf("WARCOIN\n=========\n");
 	CMonedas=Monedas();
 	PlayerOPc=Inicia();
-	printf("\nLa cantidad inicial de monedas en la pila es: %d",CMonedas);
+	printf("\n**La cantidad inicial de monedas en la pila es: %d**\n",CMonedas);
 	Comienzo(PlayerOPc,CMonedas);
 	
 	
@@ -42,12 +46,12 @@ void Comienzo(int juega,int cantmonedas){
 		int retiraJugador;
 		int retiraPC;
 		if(juega==1){
-			printf("\ningrese la cantidad de monedas que decea eliminar: ");
+			printf("\ningrese la cantidad de monedas que desea eliminar: ");
 			scanf("%d",&retiraJugador);
 			if(retiraJugador>=min && retiraJugador <= max){
 				cantmonedas=cantmonedas-retiraJugador;
-				printf("\nQuedan %d monedas ",cantmonedas);
-				juega=2;	
+				printf("Quedan %d monedas\n",cantmonedas);
+				juega=2;
 			}else {
 				printf("\nERROR: Se deben desapilar entre %d y %d monedas de la pila",min,max);
 			}
@@ -55,13 +59,21 @@ void Comienzo(int juega,int cantmonedas){
 			retiraPC=CantMonedasQueRetiraLaPc();
 			printf("\nLa computadora saco %d monedas de la pila",retiraPC);
 			cantmonedas=cantmonedas-retiraPC;
-			printf("\nQuedan %d monedas en la pila",cantmonedas);
+			printf("\nQuedan %d monedas en la pila\n",cantmonedas);
 			juega=1;
 		}
 	}
 	if(juega==1){
+		printf("\n\n");
+		system("pause");
+		system("cls");
+		printf("WARCOIN\n=========\n");
 		printf("\nLA COMPUTADORA GANO!! puntaje obtenido es: 0");
 	}else{
+		printf("\n\n");
+		system("pause");
+		system("cls");
+		printf("WARCOIN\n=========\n");
 		printf("\nGANASTE!! puntaje obtenido: 5");
 	}
 	printf("\nGracias por jugar ");
@@ -73,4 +85,12 @@ int CantMonedasQueRetiraLaPc(){
   	srand(time(0));
 	nro_azar=min+rand()%(max-min+1);
 	return nro_azar;
+}
+void Reglas(){//Consiste en una pila de monedas, de la cual el jugador y la computadora van sacando monedas hasta que ya no quedan más en la pila. El que vacía la pila de monedas es quien gana el juego
+	printf("WARCOIN\n=========\n");
+	printf("\nEl juego Consiste en una pila de monedas, de la cual el jugador y la computadora");
+	printf("\n\tvan sacando monedas hasta que ya no quedan más en la pila.");
+	printf("\n\t El que vacía la pila de monedas es quien gana el juego.\n\n");
+	system("pause");
+	system("cls");
 }
