414de91208cdae3c8077bb760e39f1a26296d454
nayuan357 <69062430+nayuan357@users.noreply.github.com>
2020-08-05 11:04:55 -0300

    Add files via upload
    Organizacion del codigo

--- MayMen.cpp
+++ MayMen.cpp
@@ -1,11 +1,13 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include <ctime>
+//prototipos
 void azar(int &numeroaleatorio);//devuelve un numero aleatorio
 void Pausa();
 void Reglas();
 bool MayMen(int Num,int NAleatorio,int &rango1,int &rango2);//aqui se revisa si el numero ingresado es mayor o menor al numero aleatorio
 void IntentosyFinal(int Naleatorio);//aqui se desarrolla el juego en si, los 10 intentos y el final
+//funcion principal
 main(){
 	int Naleatorio;
 	azar(Naleatorio);
@@ -15,22 +17,26 @@ main(){
 	printf("\n\nGracias por jugar. ");
 	Pausa();
 }
+//funcion para un numero aleatorio
 void azar(int &numeroaleatorio){
 	int nro,N=1,nro_azar; 
   	srand(time(0));
 	nro_azar=1+rand()%999;
 	numeroaleatorio=nro_azar;
 }
+//pausa
 void Pausa(){
 	printf("\n\n");
 	system("pause");
 	system("cls");
 }
+//reglas del juego
 void Reglas(){
 	printf("\t\t*********************************\n\t\t*****     Mayor y Menor     *****\n\t\t*********************************");
 	printf("\n\n\t   Este juego consiste en adivinar un numero que esta \n\tentre 1 y 999 denominado numero secreto, el cual elegire \n");
 	printf("\t de manera aleatoria. En cada intento debes ingresar un \n\tnumero hasta adivinarlo, tienes 10 intentos, mucha suerte.\n");
 }
+//funcion donde se desarrollo el juego
 bool MayMen(int Num,int NAleatorio,int &rango1,int &rango2){
 	if(Num>NAleatorio){
 		rango2=Num-1;
@@ -46,6 +52,7 @@ bool MayMen(int Num,int NAleatorio,int &rango1,int &rango2){
 				return true;
 			}
 }
+//funcion donde se almacena los intentos que van realizando. Muestra si ganaste o perdiste
 void IntentosyFinal(int Naleatorio){
 	int num_ingresado,i;
 	int rango1=1,rango2=999;
