7ed8bccc28f0a29c21b60c2cfa7c5bd3bc58e382
Romano Lautaro <lautaro.romano@alu.frt.edu.ar>
2020-08-11 18:22:58 -0300

    Update Ahorcado.cpp
    en esta version el juego detecta si se ingreso un valor no alfabetico y marca el error. otros arreglos menores.

--- Ahorcado.cpp
+++ Ahorcado.cpp
@@ -1,47 +1,53 @@
 #include<stdio.h>//prueba lautaro
 #include<stdlib.h>
 #include <ctype.h>
-int Carga(char P[25]);//Cargar la palabra para adivinar
-void PalabraAAdivinar(int cantletras,char P[25]);//muestra la palabra oculta y mmientras adivine va mostrando las letras
-void Verificacion(char Palabraparaadivinar[25],int CaracteresAdivinados[25],char letraingresada,int &muneco);//Verifica si la letra ingresada coincide con la palabra a adivinar
+int Carga();//Cargar la palabra para adivinar
+void PalabraAAdivinar();//muestra la palabra oculta y mmientras adivine va mostrando las letras
+void Verificacion(int CaracteresAdivinados[25],char letraingresada,int &muneco);//Verifica si la letra ingresada coincide con la palabra a adivinar
 void EstadoDelMuneco(int Muneco);
 void Perdio();
+void MostrarPalabra();
+
+//variable global en la que guarda la plabra oculta que se ingreso para adivinar;
+char Palabra[25];
+//variable global que guarda la cantidad de letras que tiene la palabra
+int cantletras;
+
 main(){
 	printf("AHORCADO\n========");
-	int cantletras;
-	char Palabra[25];
-	cantletras=Carga(Palabra);
-	PalabraAAdivinar(cantletras,Palabra);
+	cantletras=Carga();
+	PalabraAAdivinar();
 }
 
-int Carga(char P[25]){
+int Carga(){
 	char b;
 	int i=0,Bandera=1;
 	printf("\nIngresar la palabra a adivinar letra por letra \"EN MAYUSCULA\", Al finalizar presione 0");
 	while(Bandera!=48){//48 es el numerocero en codigo ascii
 		
 		printf("\nIngrese la %d letra: ",i+1);
-		scanf("%c",&P[i]);
+		scanf("%c",&Palabra[i]);
 		fflush(stdin);//para que no haya problemas al escanear datos tipo char
 		
 		//El siguiente codigo sirve para identeficar si un caracter ingresado es un numero o una letra
 		//-----------------------------------------------------------
-		if (isalpha(P[i])) {
+		if (isalpha(Palabra[i])) {
  		 } else {
- 		 	if (P[i]!=48){
+ 		 	if (Palabra[i]!=48){
    				printf("\nEROOR:Debe ingresar un caracter alfabetico, por favor intente nuevamente");
    				i--;
    			}
  		 }
 		//--------------------------------------------------------------
-		Bandera=P[i];
+		Bandera=Palabra[i];
 		i++;
 	}
 	system("cls");
 	return i-1;
 }
 
-void PalabraAAdivinar(int cantletras,char P[25]){
+void PalabraAAdivinar(){
+	char i=0;
 	int J=0;
 	bool Gano=true;
 	char LetraIngresada;
@@ -55,30 +61,48 @@ void PalabraAAdivinar(int cantletras,char P[25]){
 	}
 	
 	do{
+		int Bandera=1;
 		Gano=true;
 		printf("\n-------------------------------------------\n");
 		printf("Jugada #%d",J+1);
 		printf("\nIngrese una letra: ");
 		scanf("%c",&LetraIngresada);
 		fflush(stdin);//para que no haya problemas al escanear datos tipo char
-		Verificacion(P,CantLetrasAdivinadas,LetraIngresada,muneco);
-		printf("Resultado de la jugada #%d \nPalabra a adivinar: ",J+1);
-		for(int i=0;i<cantletras;i++){
-			if(CantLetrasAdivinadas[i]==1 || LetrasAdivinadas[i]==1){
-				printf("%c ",P[i]);
-				LetrasAdivinadas[i]=1;
-			}else{
-				printf("_ ");
-				Gano=false;
+		
+		//variable para verificar si la letra ingresada es un valor alfabetico
+		char aux=LetraIngresada;
+		
+		//El siguiente codigo sirve para identeficar si un caracter ingresado es un numero o una letra
+		//-----------------------------------------------------------
+		if (isalpha(aux)){
+ 		 } else {
+ 		 	printf("\nERROR: El caracter ingresado no es albabetico, intente nuevamente\n");
+ 		 	J--;
+ 		 	
+ 		 	//si la bandera esta en 0 no debe contarse la jugada
+ 		 	Bandera=0;
+ 		 	Gano=false;
+ 		 }
+		//--------------------------------------------------------------
+		if(Bandera!=0){
+			Verificacion(CantLetrasAdivinadas,LetraIngresada,muneco);
+			printf("Resultado de la jugada #%d \nPalabra a adivinar: ",J+1);
+			for(int i=0;i<cantletras;i++){
+				if(CantLetrasAdivinadas[i]==1 || LetrasAdivinadas[i]==1){
+					printf("%c ",Palabra[i]);
+					LetrasAdivinadas[i]=1;
+				}else{
+					printf("_ ");
+					Gano=false;
+				}
+			}
+			EstadoDelMuneco(muneco);
+			J++;
+			//final perdedor
+			if(muneco>=10){
+				Perdio();
 			}
 		}
-		EstadoDelMuneco(muneco);
-		J++;
-		//final perdedor
-		if(muneco>10){
-			Perdio();
-		}
-		
 	}while(Gano==false);
 	//final ganador
 	int Puntos=50-(2*muneco);
@@ -86,10 +110,10 @@ void PalabraAAdivinar(int cantletras,char P[25]){
 	system("pause");
 }
 
-void Verificacion(char Palabraparaadivinar[25],int CaracteresAdivinados[25],char letraingresada,int &muneco){
+void Verificacion(int CaracteresAdivinados[25],char letraingresada,int &muneco){
 	int B=0;
 	for(int i=0;i<25;i++){
-		if(Palabraparaadivinar[i]==letraingresada){
+		if(Palabra[i]==letraingresada){
 			CaracteresAdivinados[i]=1;
 			B=1;
 		}else CaracteresAdivinados[i]=0;
@@ -133,6 +157,18 @@ void EstadoDelMuneco(int muneco){
 }
 void Perdio(){
 	printf("\n\n=========================\nHAS PERDIDO.\nPuntaje obtenido: 0\n\n\n");
+	
+	//se muestra la palabra que debia adivinarse
+	MostrarPalabra();
+	
 	exit(0);
 	system("pause");
+}
+
+//Funcion para ostrar la palabra que debia adivinarse
+void MostrarPalabra(){
+	printf("La palabra oculta era: ");
+	for(int i=0;i<cantletras;i++){
+		printf("%c",Palabra[i]);
+	}
 }
